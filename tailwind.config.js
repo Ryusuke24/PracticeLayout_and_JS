0521ced435e1e34a31e48bb5ab87e55a40641159
Ryusuke24 <yunglean_24@mail.ru>
2023-11-01 14:15:21 +0300

    create app layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   content: ["./index.html"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        image: 'url("public/img/background.jpeg");',
+      },
+    },
   },
   plugins: [],
 };
