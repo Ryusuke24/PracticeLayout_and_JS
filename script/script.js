60d58d9213f734f9285e5d9fd492d84a98fdc8d9
Ryusuke24 <yunglean_24@mail.ru>
2023-11-01 23:58:30 +0300

    add little fixes

--- script/script.js
+++ script/script.js
@@ -1,10 +1,26 @@
 import places from "./json.js";
 
-let tripDiv = document.querySelector(".places");
+let tripDiv = document.querySelector(".quietPlaces");
+let foodTrip = document.querySelector(".foodPlaces");
+let romanticTrip = document.querySelector(".romanticPlaces");
 let rightMobileBtn = document.querySelector(".buttonRight");
 let leftMobileBtn = document.querySelector(".buttonLeft");
 let link = document.querySelector(".nextPageLink");
 let bannerId = 0;
+
+tripDiv.querySelector("h2").innerHTML = places[0].name;
+romanticTrip.querySelector("h2").innerHTML = places[1].name;
+foodTrip.querySelector("h2").innerHTML = places[2].name;
+
+let tripImg = tripDiv.querySelectorAll("img");
+let romanticImg = romanticTrip.querySelectorAll("img");
+let foodImg = foodTrip.querySelectorAll("img");
+
+for (let i = 0; i < 3; i++) {
+  tripImg[i].src = places[0].countries[i];
+  romanticImg[i].src = places[1].countries[i];
+  foodImg[i].src = places[2].countries[i];
+}
 
 leftMobileBtn.addEventListener("click", () => {
   bannerId--;
